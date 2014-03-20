fc151f2c09db98fbe9edbe916e0debe0b5e1fae7
Justin Zaun <jzaun@teampraxis.com>
2014-03-20 10:41:32 -1000

    Added assumeRole and a delete option to aws_ssl.

--- tasks/aws_ssl.js
+++ tasks/aws_ssl.js
@@ -25,9 +25,57 @@ module.exports = function(grunt) {
       configVariable: this.nameArgs
     });
 
+    var iam;
+    var done;
+
     // check for required options
     if (!options.certificateName) {
       grunt.log.error('certificateName must be defined.');
+      return;
+    }
+
+    if (options['delete'] && options['delete'] === true) {
+      done = this.async();
+      async.waterfall([
+        // Check if this should be run on another account
+        function (callback) {
+          if (!options.assumeRole) {
+            iam = new AWS.IAM();
+            grunt.verbose.writeln('No assumeRole provided');
+            callback();
+            return;
+          }
+          grunt.log.writeln('Assuming roll: ' + options.assumeRole);
+          var sts = new AWS.STS();
+          sts.assumeRole({
+            RoleArn: options.assumeRole,
+            RoleSessionName:'GruntSession'
+          }, function (err, data) {
+            if (err) {
+              callback(err);
+              return;
+            }
+            iam = new AWS.IAM({
+              region: options.region,
+              accessKeyId: data.Credentials.AccessKeyId,
+              secretAccessKey: data.Credentials.SecretAccessKey,
+              sessionToken: data.Credentials.SessionToken
+            });
+            callback();
+          });
+        },
+        function (callback) {
+          iam.deleteServerCertificate({ ServerCertificateName: options.certificateName }, callback);
+        }
+      ], function (err, data) {
+        if (err) {
+          grunt.log.error('An error occurred: ' + JSON.stringify(err, null, 2));
+          done(false);
+          return;
+        }
+        grunt.log.writeln('Removed certificate');
+        done();
+      });
       return;
     }
 
@@ -56,12 +104,40 @@ module.exports = function(grunt) {
       return;
     }
 
-    var done = this.async();
-    var iam = new AWS.IAM();
-
+    done = this.async();
     async.waterfall([
+      // Check if this should be run on another account
+      function (callback) {
+        if (!options.assumeRole) {
+          iam = new AWS.IAM();
+          grunt.verbose.writeln('No assumeRole provided');
+          callback();
+          return;
+        }
+        grunt.log.writeln('Assuming roll: ' + options.assumeRole);
+        var sts = new AWS.STS();
+        sts.assumeRole({
+          RoleArn: options.assumeRole,
+          RoleSessionName:'GruntSession'
+        }, function (err, data) {
+          if (err) {
+            callback(err);
+            return;
+          }
+          iam = new AWS.IAM({
+            region: options.region,
+            accessKeyId: data.Credentials.AccessKeyId,
+            secretAccessKey: data.Credentials.SecretAccessKey,
+            sessionToken: data.Credentials.SessionToken
+          });
+          callback();
+        });
+      },
+
       // check if it already exists
-      _.bind(iam.listServerCertificates, iam, {}),
+      function (callback) {
+        iam.listServerCertificates(callback);
+      },
       function (data, callback) {
         var cert = _.findWhere(data.ServerCertificateMetadataList, { ServerCertificateName: options.certificateName });
         if (cert) {
