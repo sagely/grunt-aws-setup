92ff5cf0748cd146c1626bebf8908156b86b8004
Justin Zaun <jzaun@teampraxis.com>
2014-03-20 11:27:59 -1000

    Add assumeRole to aws_s3_cors

--- tasks/aws_s3_cors.js
+++ tasks/aws_s3_cors.js
@@ -37,18 +37,51 @@ module.exports = function(grunt) {
     }
 
     var done = this.async();
-    var s3 = new AWS.S3({ region: options.region });
+    var s3;
 
-    s3.putBucketCors({
-      Bucket: options.bucketName,
-      CORSConfiguration: options.corsConfiguration
-    }, function (err, data) {
+    async.waterfall([
+      // Check if this should be run on another account
+      function (callback) {
+        if (!options.assumeRole) {
+          s3 = new AWS.S3({ region: options.region });
+          grunt.verbose.writeln('No assumeRole provided');
+          callback();
+          return;
+        }
+        grunt.verbose.writeln('Assuming roll: ' + options.assumeRole);
+        var sts = new AWS.STS();
+        sts.assumeRole({
+          RoleArn: options.assumeRole,
+          RoleSessionName:'GruntSession'
+        }, function (err, data) {
+          if (err) {
+            callback(err);
+            return;
+          }
+          s3 = new AWS.S3({
+            region: options.region,
+            accessKeyId: data.Credentials.AccessKeyId,
+            secretAccessKey: data.Credentials.SecretAccessKey,
+            sessionToken: data.Credentials.SessionToken
+          });
+          callback();
+        });
+      },
+
+      // check to see if the stack exists and create or update it
+      function (callback) {
+        s3.putBucketCors({
+          Bucket: options.bucketName,
+          CORSConfiguration: options.corsConfiguration
+        }, callback);
+      }
+    ], function (err, data) {
       if (err) {
         grunt.log.error('An error occurred: ' + JSON.stringify(err, null, 2));
         done(false);
         return;
       }
-      done();      
+      done();
     });
   });
 };
